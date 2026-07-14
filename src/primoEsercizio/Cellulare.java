feb9d0b809c59f8e6014e2d9e8d385c4ad3e4d40
Francesco <61A@A61>
2026-07-14 11:57:10 +0200

    Aggiornamento con esercizi

---
+++ src/primoEsercizio/Cellulare.java
@@ -0,0 +1,85 @@
+package primoEsercizio;
+
+public class Cellulare {
+	private double creditoDisponibile;
+	private int chiamateEffettuate;
+	private double tariffa;
+
+	public void ricaricaCellulare(int importo) {
+		if (importo == 5 || importo == 10 || importo == 25) {
+			creditoDisponibile += importo;
+			System.out.println("--------------------------------");
+			System.out.println("Ricarica effettuata con un importo di: " + importo);
+			System.out.println("--------------------------------");
+		} else {
+
+			System.out.println("--------------------------------");
+			System.out.println("Importo non Valido");
+			System.out.println("--------------------------------");
+
+		}
+	}
+
+	public void impostaTariffa(double centesimiAlMinuto) {
+		tariffa = centesimiAlMinuto / 100.0;
+		System.out.println("--------------------------------");
+		System.out.println("Tariffa Impostata a " + tariffa + "/minuto");
+		System.out.println("--------------------------------");
+	}
+
+	public void effettuaChiamata(String numero, int minuti) {
+		int lunghezzanumero = numero.length();
+		if (tariffa <= 0 || creditoDisponibile <= 0) {
+			System.out.println("Il Credito non è sufficiente o la tariffa non è corretta");
+		} else {
+			if (lunghezzanumero == 10) {
+				double costoTeorico = minuti * tariffa;
+
+				if (costoTeorico <= creditoDisponibile) {
+
+					System.out.println("--------------------------------");
+					creditoDisponibile -= costoTeorico;
+					chiamateEffettuate++;
+					System.out.println("Hai effettuato una chiamata al numero: " + numero);
+					System.out.println("--------------------------------");
+				} else {
+
+					System.out.println("--------------------------------");
+					System.out.println("Credito Insufficiente");
+					System.out.println("--------------------------------");
+
+				}
+			}
+			else {
+				System.out.println("--------------------------------");
+				System.out.println("Numero Non Valido, Il numero deve essere di 15 caratteri");
+				System.out.println("--------------------------------");
+
+			}
+
+		}
+
+	}
+
+	public void stampaCreditoDisponibile() {
+
+		System.out.println("--------------------------------");
+		System.out.println("Il Tuo credito disponibile attuale è di: " + creditoDisponibile);
+		System.out.println("--------------------------------");
+	}
+
+	public void stampaChiamateEffettuate() {
+
+		System.out.println("--------------------------------");
+		System.out.println("Numero chiamate effettuate: " + chiamateEffettuate);
+		System.out.println("--------------------------------");
+	}
+
+	public void azzeraChiamate() {
+		chiamateEffettuate = 0;
+		System.out.println("--------------------------------");
+		System.out.println("Numero Chiamate Azzerato");
+		System.out.println("--------------------------------");
+		stampaChiamateEffettuate();
+	}
+}
---
+++ src/primoEsercizio/CellulareTest.java
@@ -0,0 +1,78 @@
+package primoEsercizio;
+
+import java.util.Scanner;
+
+public class CellulareTest {
+
+	public static void main(String[] args) {
+		Scanner n = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
+		Cellulare cel = new Cellulare();
+		boolean spegni = false;
+		
+		while(!spegni)
+		{
+			System.out.println("--------------------------------");
+			System.out.println("Benvenuto nel Cellulare ");
+			System.out.println("Scelta 1: Ricarica");
+			System.out.println("Scelta 2: Imposta Tariffa");
+			System.out.println("Scelta 3: Chiamare(con credito sufficiente)");
+			System.out.println("Scelta 4: Visualizza Credito Disponibile");
+			System.out.println("Scelta 5: Visualizza Quantità Chiamate effettuate");
+			System.out.println("Scelta 6: Azzera Chiamate");
+			System.out.println("Scelta 7: Spegni Cellulare");
+			System.out.println("Inserisci Scelta:");
+			
+			int scelta = n.nextInt();
+			switch(scelta)
+			{
+			case 1:
+				System.out.println("Inserisci Importo Da Ricaricare (5€-10€-25€ valori accettati)");
+				int importoRicarica = n.nextInt();
+				cel.ricaricaCellulare(importoRicarica);
+				break;
+			case 2:
+				System.out.println("Inserisci Tariffa: Centesimi al minuto");
+				double sceltaTariffa = n.nextInt();
+				cel.impostaTariffa(sceltaTariffa);
+				break;
+			case 3:
+				System.out.println("Inserisci Il Numero da chiamare:");
+				String numero = s.nextLine();
+				System.out.println("Inserisci Minuti Necessari:");
+				int minuti = n.nextInt();
+				cel.effettuaChiamata(numero, minuti);
+				break;
+			case 4:
+				cel.stampaCreditoDisponibile();
+				break;
+			case 5:
+				cel.stampaChiamateEffettuate();
+				break;
+			case 6:
+				cel.azzeraChiamate();
+				break;
+			case 7:
+				System.out.println("Il telefono è stato spento");
+				spegni = true;
+				break;
+			default:
+				System.out.println("Scelta non corretta");
+				break;
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		s.close();
+		n.close();
+		
+		
+	}
+
+}
